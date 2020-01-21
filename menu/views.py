bb886359a53bfba0d20bcc8395afb52e950233d5
Redsnip8 <FCipolone@gmail.com>
2020-01-20 22:08:24 -0500

    update 2.6.12
    Base menu url function. Models to show in next commit

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  return render(request, 'menu/menu.html')
+
+def item(request):
+  return render(request, 'menu/nutrition.html')
