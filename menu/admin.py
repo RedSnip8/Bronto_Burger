ce8953ae52f0452d1cf76cb0096b7d485986a4c5
Redsnip8 <FCipolone@gmail.com>
2020-01-22 14:48:58 -0500

    Update 2.6.02
    Menu Models finialized to project needs. Next update will have content
    dynamically pulled into menu page and other pages.

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class ItemsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'promoted', 'kids_menu', 'cals')
+    list_display_links = ('id', 'name')
+    list_filter = ('category',)
+    list_editable = ('category','promoted', 'kids_menu')
+    search_fields = ('name', 'category', 'cals')
+    list_per_page = 25
+
+tables = (
+  models.Lactose_Free,
+  models.Nut_Free,
+  models.Soy_Free,
+  models.Egg_Free,
+  models.Gluten_Free,
+  models.Vegan,
+  models.Vegetarian
+)
+
+admin.site.register(models.Items, ItemsAdmin)
+for x in tables:
+  admin.site.register(x)
--- menu/models.py
+++ menu/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 class Items(models.Model):
+    food_types = (
+        ('entr', 'Entree'),
+        ('side', 'Side'),
+        ('dsrt', 'Dessert'),
+        ('bev', 'Beverage'),
+    )
+
     name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, choices=food_types, default='entr')
     slogan = models.CharField(max_length=200)
     description = models.CharField(max_length=350)
-    ingredients = models.CharField(max_length=500)
+    ingredients = models.CharField(max_length=1000)
     cals = models.IntegerField()
     fat = models.IntegerField()
     sat_fat = models.IntegerField()
@@ -15,49 +23,51 @@ class Items(models.Model):
     fiber = models.IntegerField()
     sugar = models.IntegerField()
     protien = models.IntegerField()
+    promoted = models.BooleanField(default=False)
     kids_menu = models.BooleanField(default=False)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = "Items"
 
 class Vegan(models.Model):
     item = models.ForeignKey(Items, on_delete=models.DO_NOTHING)
     def __str__(self):
-        return self.item
-
+        return self.item.name
 
 class Vegetarian(models.Model):
     item = models.ForeignKey(Items, on_delete=models.DO_NOTHING)
     def __str__(self):
-        return self.item
+        return self.item.name
 
 class Gluten_Free(models.Model):
     item = models.ForeignKey(Items, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return self.item
+        return self.item.name
 
 class Lactose_Free(models.Model):
     item = models.ForeignKey(Items, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return self.item
+        return self.item.name
 
 class Egg_Free(models.Model):
     item = models.ForeignKey(Items, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return self.item
+        return self.item.name
 
 class Soy_Free(models.Model):
     item = models.ForeignKey(Items, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return self.item
+        return self.item.name
 
-class Nute_Free(models.Model):
+class Nut_Free(models.Model):
    item = models.ForeignKey(Items, on_delete=models.DO_NOTHING)
 
    def __str__(self):
-      return self.item
+      return self.item.name
