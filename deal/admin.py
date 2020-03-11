d807dbc4515cae9852f472e439c309b2d1e57f95
Redsnip8 <FCipolone@gmail.com>
2020-03-10 22:55:14 -0400

    Update 2.6.56
    These updates are barely numbered accurately. This update is for the deals
    app. We have added the information for each model field. Next updates will
    include ad photo storage and the admin will be adjusted and tested for
    usablitiy.

--- deal/admin.py
+++ deal/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class OfferAdmin(admin.ModelAdmin):
+	list_display = ('id', 'offer_name', 'start_date', 'end_date',)
+	list_display_links = ('id', 'offer_name',)
+	list_filter = ('offer_name',)
+	#list_editable = ('',)
+	#search_fields = ('name',)
+	list_per_page = 25
+
+admin.site.register(models.Offer, OfferAdmin)
--- deal/models.py
+++ deal/models.py
@@ -2,6 +2,27 @@ from django.db import models
 from datetime import date
 from django.utils.translation import gettext as _
 class Offer(models.Model):
+    categories = [
+      ('NF', 'New Food'),
+      ('MD', 'Meal Deal'),
+      ('DC', 'Discounts'),
+      ('HD', 'Holiday'),
+      ('OT', 'Other'),
+    ]
+
+    audience = [
+      ('Kds', 'Kids'),
+      ('Tns', 'Teens'),
+      ('Ads', 'Adults'),
+      ('Eds', 'Elderly'),
+      ('Fam','Family'),
+    ]
+
     offer_name = models.CharField(max_length=80)
-    start_date = models.DateField(_("Date"), default=date.today)
-    end_date = models.DateField(_("Date"))
+    start_date = models.DateField(_("Start Date"), default=date.today)
+    end_date = models.DateField(_("End Date"))
+    audience = models.CharField(max_length=100, choices=audience)
+    budget = models.DecimalField(max_digits=30, decimal_places=2)
+    description = models.CharField(max_length=500)
+    category = models.CharField(max_length=100, choices=categories)
+
